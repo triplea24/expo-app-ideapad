aac2688f04ca05c77f87b55bf270883e2117c203
Amir Ahangari <triplea1373@gmail.com>
2018-07-17 10:11:46 +0430

    initialise firebase in App.js

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+import firebase from 'firebase';
+
 import LoginForm from './src/components/login-form';
+import Header from './src/components/Header';
+
+// NOTE: Create a firebase.js file inside the project directory and export the config variable there!!!
+import config from './firebase';
 
 export default class App extends React.Component {
+  componentDidMount(){
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <Header />
         <LoginForm />
       </View>
     );
@@ -16,7 +27,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
